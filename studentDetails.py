b4002b3db50009d08d72518f6eb5294ab08e8586
devil12-tech <nikhilpujar3@gmail.com>
2025-10-28 11:35:36 +0530

    save changes file

--- studentDetails.py
+++ studentDetails.py
@@ -1,7 +1,9 @@
-name = input("Enter Student name:")
-id = int(input("Enter Student Id: "))
+# Collect student details from user input
+name = input("Enter Student Name: ")
+id = int(input("Enter Student ID: "))
 clg_name = input("Enter Student College Name: ")
 
+# Display the collected information
 print(f"College Name: {clg_name}")
-print(f" Student Id: {id}")
+print(f"Student ID: {id}")
 print(f"Student Name: {name}")
