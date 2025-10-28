f9a2715e3b71df7f877016344137388fc53c8458
devil12-tech <nikhilpujar3@gmail.com>
2025-10-28 11:29:13 +0530

    save second file

---
+++ studentDetails.py
@@ -0,0 +1,7 @@
+name = input("Enter Student name:")
+id = int(input("Enter Student Id: "))
+clg_name = input("Enter Student College Name: ")
+
+print(f"College Name: {clg_name}")
+print(f" Student Id: {id}")
+print(f"Student Name: {name}")
