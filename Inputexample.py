9414a64bac4066c7aee77290385457c11c49267d
devil12-tech <nikhilpujar3@gmail.com>
2025-11-04 11:02:07 +0530

    Changes

--- Inputexample.py
+++ Inputexample.py
@@ -1,7 +1,7 @@
 import sys
 
-name = sys.argv[1]      # First command-line argument (after script name)
-rollno = sys.argv[2]    # Second command-line argument
+name = sys.argv[0]      # First command-line argument (after script name)
+rollno = sys.argv[1]    # Second command-line argument
 
 print(f"Name: {name}\n")        # Prints the name with a newline
 print(f"Roll No : {rollno}")    # Prints the roll number
