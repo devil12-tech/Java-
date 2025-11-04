6504809512dcd92e6c67cced9fd3259f75011355
devil12-tech <nikhilpujar3@gmail.com>
2025-11-04 11:33:53 +0530

    Fix input handling and correct print statements

--- ImportInput.py
+++ ImportInput.py
@@ -1,15 +1,16 @@
-import sys 
-if len(sys.argv) == 3:
-  script_name = sys.argv[0]
-  name = sys.arv[1]
-  rollno = sys.argv[2]
-  print("User Provided input values")
-else: 
-   script_name =sys.argv[0]
-   name = "Nikhil"
-   rollno = "004"
-   print9"User didn't provide any input")
+import sys
 
-pint("Script Name:",script_name)
-print("Student Name: ",name)
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("User Provided input values")
+else:
+    script_name = sys.argv[0]
+    name = "Nikhil"
+    rollno = "004"
+    print("User didn't provide any input")
+
+print("Script Name:", script_name)
+print("Student Name:", name)
 print("Roll Number:", rollno)
